965a964ffced299abec3f3581f76979a36b1f9bd
Rarwemo <jamesngangai290@gmail.com>
2025-02-19 11:44:03 +0300

    Homepage made

---
+++ src/Views/Homepage.js
@@ -0,0 +1,22 @@
+// src/Views/Homepage.js
+import React, { useState } from 'react';
+import './Homepage.css';
+
+const Homepage = () => {
+    const [isBlue, setIsBlue] = useState(false);
+
+    const toggleColor = () => {
+        setIsBlue(!isBlue);
+    };
+
+    return (
+        <div className="gasusage-homepage">
+            <h1>Welcome to your Gas Usage Website</h1>
+            <p>The trusted site in manageing your gas usage</p>
+            <div className={`gasusage-color-box ${isBlue ? 'gasusage-blue' : 'gasusage-red'}`}></div>
+            <button className="gasusage-button" onClick={toggleColor}>Change Color</button>
+        </div>
+    );
+};
+
+export default Homepage;
