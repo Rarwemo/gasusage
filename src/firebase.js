2fec9e9570c53a838209f5b1f1fb0071e78195ae
Rarwemo <jamesngangai290@gmail.com>
2025-02-20 09:36:25 +0300

    Front and Backend

--- src/firebase.js
+++ src/firebase.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBMaozqMSeRjoskXW-fvhB7Y3G0r4wqWd8",
-  authDomain: "gasusage-afd48.firebaseapp.com",
-  projectId: "gasusage-afd48",
-  storageBucket: "gasusage-afd48.appspot.com",
-  messagingSenderId: "50951784637",
-  appId: "1:50951784637:web:92b4dc5b3c817585430fd4",
-  measurementId: "G-GR3V4EJGM2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
